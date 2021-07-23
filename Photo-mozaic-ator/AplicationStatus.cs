945ad9c6f9e8a2bcbe8d96732321d6f33edfedfe
LukasCaha <lukas.caha@outlook.com>
2021-07-23 14:28:21 +0200

    Aplication status comments

--- Photo-mozaic-ator/AplicationStatus.cs
+++ Photo-mozaic-ator/AplicationStatus.cs
@@ -17,17 +17,35 @@ namespace Photo_mozaic_ator
         public static string newTilesetDir;
         public static string existingTilesetDir;
         public static Bitmap outputImage { get; set; }
+        /// <summary>
+        /// Scales input image so result has more tiles.
+        /// </summary>
         public static double imageScale { get; set; }
+        /// <summary>
+        /// Setting to ignore black pixels. Suiting for tilesets with tiles with black boackground/alpha.
+        /// </summary>
         public static int ignoreBlackPixels { get; set; }
+        /// <summary>
+        /// Adds input image to output to see difference of before/after.
+        /// </summary>
         public static bool beforeAfterComparation { get; set; }
-
-        //tile generation
+        /// <summary>
+        /// Tile size in pixels. Always square.
+        /// </summary>
         public static int tileSize { get; set; }
+        /// <summary>
+        /// <para>Snaps colors to smaller color pallete to limit number of possible colors/existing tiles.</para>
+        /// </summary>
         public static double snappingFactor { get; set; }
+        /// <summary>
+        /// Color distance Strategy. Determines which method of calculating distance use.
+        /// </summary>
         public static IDistanceStrategy strategy { get; set; }
 
+        #region Methods
         static AplicationStatus()
         {
+            //default values
             workingDirectory = @"./";
             inputFile = null;
             outputFile = null;
@@ -41,6 +59,10 @@ namespace Photo_mozaic_ator
             strategy = new SquareDistanceStrategy();
         }
 
+        /// <summary>
+        /// If output file is not defined, create random name to always save the output image.
+        /// </summary>
+        /// <returns></returns>
         public static string GetOutputFile()
         {
             if (outputFile == null)
@@ -50,23 +72,14 @@ namespace Photo_mozaic_ator
             return outputFile;
         }
 
-        public static string GetExistingTilesetDir()
-        {
-            if (existingTilesetDir == null)
-            {
-                return workingDirectory + @"faces by color down 4";
-            }
-            return existingTilesetDir;
-        }
-
+        /// <summary>
+        /// Setter for color distance strategy.
+        /// </summary>
+        /// <param name="_strategy"></param>
         public static void SetColorDistanceStrategy(IDistanceStrategy _strategy)
         {
             strategy = _strategy;
         }
-
-        public static void SetOutputImage(Bitmap _outputImage)
-        {
-            outputImage = new Bitmap(_outputImage);
-        }
+        #endregion
     }
 }
